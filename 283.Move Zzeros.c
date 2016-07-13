6df6a045e3fbac8af2ed86788d3bec3e965a8592
Trim21 <niuziru21@hotmail.com>
2016-07-13 11:41:28 +0800

    283 just a start

---
+++ 283.Move Zzeros.c
@@ -0,0 +1,20 @@
+/*
+   283. Move Zeroes  QuestionEditorial Solution  My Submissions
+   Total Accepted: 96970
+   Total Submissions: 214249
+Difficulty: Easy
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+*/
+void moveZeroes(int* nums, int numsSize) {
+    for(int i;i<=numsSize;i++){
+        
+    }
+        
+
+}
